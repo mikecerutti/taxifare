4fba6aa8cb597dd11bcbb2bb1ffa4c69451611ab
mikecerutti <mike.cerutti@gmail.com>
2023-06-02 14:26:36 +0800

    update datetime

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import datetime
 # Taxi Fare Prediction
 """
 
-pu_date = st.date_input('Pick-up Date', value=datetime.datetime.today().date)
+pu_date = st.date_input('Pick-up Date', value=datetime.datetime.today())
 pu_time = st.time_input('Pick-up Time', value=datetime.time(12))
 pu_lat = st.number_input('Pick-up Latitude', value=40.165220)
 pu_long = st.number_input('Pick-up Longitude', value=-83.062350)
